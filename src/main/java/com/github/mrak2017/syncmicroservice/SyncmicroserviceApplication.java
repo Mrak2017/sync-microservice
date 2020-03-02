29fa4767dd28dae96e0dfa87b4c4e785d68b2789
Mike <redrik09@gmail.com>
2020-03-02 19:37:11 +0700

    spring boot initializr init

---
+++ src/main/java/com/github/mrak2017/syncmicroservice/SyncmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.github.mrak2017.syncmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mrak2017/syncmicroservice/SyncmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mrak2017.syncmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
